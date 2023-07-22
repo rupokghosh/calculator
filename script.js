6ca1b50a212cff3e48b30b131cf69dca1fe60a6c
Rupok ghosh <rupokghosh291@gmail.com>
2023-07-22 14:13:20 -0700

    add loop to get numbers

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
 //variables
 
+const screen = document.getElementById("screen");
+const numbers = document.querySelectorAll(".number");
+const AC = document.getElementById("AC");
+const backspace = document.getElementById("backspace");
+const equals = document.getElementById("equals");
+
 //functions
 
-clearDisplay(){
+function clearDisplay(){
+
+    screen.textContent = '';
+}
+
+function backspaceFunction(){
 
 }
 
-backspace(){
+function appendValues(value){
+    screen.innerText = value;
+}
+
+function operationChooser(){
 
 }
 
-appendValues(){
+function calculate(){
 
 }
 
-operationChooser(){
 
-}
+// Calculator funtionality
 
-calculate(){
+// loop through the buttons to find out which number is pressed
 
-}
+numbers.forEach(input => {
+    input.addEventListener('click', function(){
+        appendValues(this.value);
+    });
+})
