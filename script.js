f441cb87f60af95412a4cd81d6b84124efda8848
Rupok ghosh <rupokghosh291@gmail.com>
2023-07-23 01:50:03 -0700

    add operationChooser fucntion

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 const screen = document.getElementById("screen");
 const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
 const AC = document.getElementById("AC");
 const backspace = document.getElementById("backspace");
 const equals = document.getElementById("equals");
@@ -22,13 +23,17 @@ function appendValues(value){
     screen.innerText = screen.innerText + value;
 
 }
-
-function operationChooser(){
-
+function operationChooser(operation){
+    if(operation === '+' && screen.innerText.includes('+')) return;
+    if(operation === '-' && screen.innerText.includes('-')) return;
+    if(operation === '*' && screen.innerText.includes('*')) return;
+    if(operation === '/' && screen.innerText.includes('/')) return;
+    screen.innerText =screen.innerText + operation;
+    return operation;
 }
 
 function calculate(){
-
+    
 }
 
 
@@ -42,10 +47,18 @@ AC.addEventListener('click', ()=>{
 
 // backspace button 
 
-// loop through the buttons to find out which number is pressed
+// loop to find out which number is pressed
 
 numbers.forEach(input => {
     input.addEventListener('click', function(){
         appendValues(this.value);
+    });
+})
+
+//loop to find out which operator is pressed
+
+operators.forEach(input => {
+    input.addEventListener('click', function() {
+        operationChooser(this.value);
     });
 })
